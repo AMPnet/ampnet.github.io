06f128b80faaeca21931a0d1ab810129c914868e
Matija Pevec <pevec@live.com>
2021-03-09 22:42:29 +0100

    mitcho changes

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ $(document).ready(() => {
 document.addEventListener("scroll", function () {
     var st = window.pageYOffset || document.documentElement.scrollTop;
     $("nav").css("background-color", "rgba(255,255,255," + (st / 200).toString() + ")")
+    var offset = 80 - st;
+    if(offset < 0) {
+        offset = 0
+    }
+    $("#tbnav").css("margin-top", offset.toString() + "px")
 }, false)
 
 $('.navbar-nav>li>a').on('click', function () {
